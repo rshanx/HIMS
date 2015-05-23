ce737a78df59f28b8ec812e0ea3672c963740ce3
vvkpr8993 <rpDyXTAKB8f94j>
2015-05-23 15:42:00 +0530

    Encrypted Pwd login

--- HIMS/MainWindow.xaml.cs
+++ HIMS/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using HIMS.BLL;
+using HIMS.Transaction.Reception;
+using System.Configuration;
 
 namespace HIMS
 {
@@ -24,17 +29,63 @@ namespace HIMS
         {
             InitializeComponent();
         }
-
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["HIMS.Properties.Settings.HIMSConnectionString"].ConnectionString);
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        DataTable dt;
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-         //   MessageBox.Show("hello");
-            ReportForm obj = new ReportForm();
-            obj.Show();
+            #region commented
+            //   MessageBox.Show("hello");
+         //   ReportForm obj = new ReportForm();
+            //   obj.Show();
+            #endregion
+            Encryption enc = new Encryption();
+            string userid = tbuser.Text.ToString();
+            string pwd =  enc.encrypt(tbpwd.Password.ToString());
+            bool result = logincheck(userid, pwd);
+            if (result)
+            {
+                Home objhome = new Home();
+                this.Hide();
+                objhome.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong UserID/Password ");
+            }
         }
+
+        private bool logincheck(string userid, string pwd)
+        {
+            string q = "select *  from user_mst where user_id='"+userid+"' and password='"+pwd+"' ";
+            cmd = new SqlCommand(q,con);
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            ///throw new NotImplementedException();
+        }
+
+        #region other events
 
         private void tb1_TextChanged(object sender, TextChangedEventArgs e)
         {
+          ///  MessageBox.Show("text changed");
+        }
 
+        private void tbuser_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+        //    MessageBox.Show("selection changed");
         }
 
 
@@ -42,5 +93,6 @@ namespace HIMS
         //{
         //    MessageBox.Show("Designed By VIVEK AP");
         //}
+        #endregion
     }
 }
