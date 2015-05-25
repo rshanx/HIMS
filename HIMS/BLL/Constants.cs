5cc3c5f3b242742a327e00046bb95bee89f44d91
vvkpr8993 <rpDyXTAKB8f94j>
2015-05-25 19:30:43 +0530

    Reception Module added by vivek

---
+++ HIMS/BLL/Constants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BLL
+{
+    class Constants
+    {
+        #region hospital_id
+        public const string hospial_id = "1";
+        #endregion
+
+        #region Yes/No
+        public const String FLAG_Y = "Y";
+        public const String FLAG_N = "N";
+        #endregion //Yes/No
+
+        #region Active/Deactive
+        public const String ACTIVE_STATUS = "A";
+        public const String DEACTIVE_STATUS = "D";
+        #endregion //Active/Deactive
+
+        #region Credit/Debit
+        public const String CREDIT = "C";
+        public const String DEBIT = "D";
+        #endregion //Credit/Debit
+
+        #region Organization Types
+        public const String ORG_TYPE_COMPANY = "COM";
+        public const String ORG_TYPE_INSURANCE = "INS";
+        #endregion //Organization Types
+
+
+
+    }
+}
--- HIMS/MainWindow.xaml.cs
+++ HIMS/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
 using HIMS.BLL;
+using HIMS.XSD;
 using HIMS.Transaction.Reception;
 using System.Configuration;
 
@@ -30,10 +31,10 @@ namespace HIMS
             InitializeComponent();
         }
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["HIMS.Properties.Settings.HIMSConnectionString"].ConnectionString);
-        SqlCommand cmd;
-        SqlDataAdapter da;
+        SqlCommand cmd,cmd_license;
+        SqlDataAdapter da,da_license;
         DataSet ds;
-        DataTable dt;
+        DataTable dt,dt_license;
         
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -42,25 +43,116 @@ namespace HIMS
          //   ReportForm obj = new ReportForm();
             //   obj.Show();
             #endregion
-            Encryption enc = new Encryption();
-            string userid = tbuser.Text.ToString();
-            string pwd =  enc.encrypt(tbpwd.Password.ToString());
-            bool result = logincheck(userid, pwd);
-            if (result)
+
+            //#region Verify licensing
+            //bool is_first_time_login;
+            //try
+            //{
+            //    string h_id = Constants.hospial_id;
+            //    string q = "select * from license where hospital_id ='"+h_id+"' ";
+            //    con.Open();
+            //    cmd_license = new SqlCommand(q, con);
+            //    da_license = new SqlDataAdapter(cmd_license);
+            //    dt_license = new DataTable();
+            //    da_license.Fill(dt_license);
+            //    con.Close();
+            //    if(dt_license.Rows.Count ==1)
+            //    {
+            //        is_first_time_login = false;
+            //    }
+            //    else if(dt_license.Rows.Count <= 0)
+            //    {
+            //        ClientInformation obj = new ClientInformation();
+            //        is_first_time_login = false;//is first time is true but we have to set in table false
+            //        //DataRow row = DS_license.licenseRow;
+            //        //DS_license.dt_licenseRow row = DS_license.dt_licenseRow();
+            //        DateTime d1 = DateTime.Now;
+            //        DateTime d2 = d1.AddDays(30);
+            //        string is_purchased = Constants.FLAG_N;
+            //        string cancel_flag = Constants.FLAG_N;
+            //        string created_by = "VIVEK";
+            //        DateTime created_date =  d1;
+            //        string created_host = "VIVEK-PC";
+            //        string is_first;
+            //        if(is_first_time_login)
+            //        {
+            //             is_first = Constants.FLAG_Y;
+            //        }
+            //        else
+            //        {
+            //            is_first = Constants.FLAG_N;
+            //        }
+            //        string ins_q = " INSERT INTO LICENSE ([hospital_id],[is_first_time],[trial_start_date],[trial_end_date],[is_purchased],[cancel_flag],[created_by],[created_date],[created_host]) values ('" + h_id + "','" + is_first + "',CONVERT(VARCHAR,'" + d1 + "',103),CONVERT(VARCHAR,'" + d2 + "',103),'" + Constants.FLAG_N + "','" + cancel_flag + "',CONVERT(VARCHAR,'" + created_date + "',103),'" + created_by + "','" + created_host + "') ";
+
+            //        con.Open();
+            //        SqlCommand cmd_ins = new SqlCommand(ins_q, con);
+            //        int i = cmd_ins.ExecuteNonQuery();
+            //        if (i == 1)
+            //        {
+            //            MessageBox.Show("your trial period start for 30 days");
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("error while updating trial period in license table");
+            //        }
+            //        con.Close();
+            //    }
+            //}
+            //catch(Exception exc)
+            //{
+            //    MessageBox.Show("error :\n" + exc.Message);
+            //}
+            //#endregion
+
+            #region Check Latest Version
+            #endregion
+
+            #region Initial Validation
+            if (tbuser.Text == string.Empty)
             {
-                Home objhome = new Home();
-                this.Hide();
-                objhome.Show();
+                MessageBox.Show("Please enter Username");
+                return;
             }
-            else
+            if(tbpwd.Password == string.Empty)
             {
-                MessageBox.Show("Wrong UserID/Password ");
+                MessageBox.Show("Please enter Password");
+                return;
             }
+            #endregion
+
+            #region Login Check
+            try
+            {
+                Encryption enc = new Encryption();
+                string userid = tbuser.Text.ToString();
+                string pwd = enc.encrypt(tbpwd.Password.ToString());
+                bool result = logincheck(userid, pwd);
+
+                if (result)
+                {
+                    //Home objhome = new Home();
+                    Reception objhome = new Reception();
+                    this.Hide();
+                    objhome.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong UserID/Password ");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occurred" + ex.Message);
+            }
+            #endregion
+            
         }
 
         private bool logincheck(string userid, string pwd)
         {
             string q = "select *  from user_mst where user_id='"+userid+"' and password='"+pwd+"' ";
+            con.Close();
+            con.Close();
             cmd = new SqlCommand(q,con);
             da = new SqlDataAdapter(cmd);
             dt = new DataTable();
@@ -86,6 +178,11 @@ namespace HIMS
         private void tbuser_SelectionChanged(object sender, RoutedEventArgs e)
         {
         //    MessageBox.Show("selection changed");
+        }
+
+        private void Login_Closed(object sender, EventArgs e)
+        {
+            
         }
 
 
