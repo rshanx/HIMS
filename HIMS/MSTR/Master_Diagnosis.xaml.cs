3b6f73de160a5fa99f5941d0ea37c0b94afe065c
sdarshil9 <sdarshil9@gmail.com>
2015-05-27 19:16:05 +0530

    VIVEKHP-Master

---
+++ HIMS/MSTR/Master_Diagnosis.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HIMS.MSTR
+{
+    /// <summary>
+    /// Interaction logic for Master_Diagnosis.xaml
+    /// </summary>
+    public partial class Master_Diagnosis : Window
+    {
+        public Master_Diagnosis()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HIMS/MSTR/Master_Medicine.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HIMS.MSTR
+{
+    /// <summary>
+    /// Interaction logic for Master_Medicine.xaml
+    /// </summary>
+    public partial class Master_Medicine : Window
+    {
+        public Master_Medicine()
+        {
+            InitializeComponent();
+        }
+    }
+}
